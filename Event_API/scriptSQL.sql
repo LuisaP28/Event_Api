b1ff89eaaebabedea95e57b0ec88bc2dd71a9b31
LuisaP28 <151669754+LuisaP28@users.noreply.github.com>
2024-04-26 09:31:01 -0500

    Taller terminado

---
+++ Event_API/scriptSQL.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE event_api;
+
+USE event_api;
+
+[
+    {
+        "id": "e25ca60e-d421-4e78-9516-8d77df70e944",
+        "name": "Musica y danza",
+        "date": "2025-06-02",
+        "location": "Universidad de Antioquia",
+        "capacity": 800
+    },
+    {
+        "id": "5153d4ac-4ac8-49c8-9cba-54e6a2ef92b0",
+        "name": "Festival del cine",
+        "date": "2024-12-24",
+        "location": "Parque de los deseos",
+        "capacity": 100
+    },
+    {
+        "id": "f72464dc-9657-4d7b-8b03-17c811a85195",
+        "name": "Burguer Master",
+        "date": "2024-08-15",
+        "location": "Cc Santafe",
+        "capacity": 150
+    },
+    {
+        "id": "d1e7b431-48a4-4992-80e1-d55626cf0cab",
+        "name": "Feria del libro",
+        "date": "2024-11-22",
+        "location": "Jardín Botánico",
+        "capacity": 500
+    },
+    {
+        "id": "80bbfc8d-ace2-45f0-88c2-838385c65195",
+        "name": "Concierto Enanitos verdes",
+        "date": "2024-06-10",
+        "location": "Estadio",
+        "capacity": 1000
+    }
+]
